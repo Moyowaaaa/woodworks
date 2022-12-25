166f9308952f4e15bed02ccda7e4cb9a15004670
Moyowaaaa <moyowa7@icloud.com>
2022-12-25 19:25:40 +0100

    🚀

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+  <div class="h-screen w-full flex items-center justify-center bg-black text-white text-4xl">
+    <h1>Vue 3 + tailwind</h1>
+  </div>
+  </main>
+</template>
